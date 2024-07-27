029de95d3673fad9e9f7d3cc1dd4d7ffedeab4ba
arunkhairwar <arunkhairwar66@gmail.com>
2024-07-27 10:00:27 +0530

    creatd mini boll which follow the cursor

---
+++ app.js
@@ -0,0 +1,13 @@
+// import LocomotiveScroll from 'locomotive-scroll';
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+}); 
+
+function circleMouseFollower() {
+    window.addEventListener("mousemove", function(dets){
+    document.querySelector("#miniCircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
+    })
+} 
+circleMouseFollower();
